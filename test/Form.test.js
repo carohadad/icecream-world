512b3220a1f72402928576e16c4a4984ee69e329
carohadad <carolinahadad@gmail.com>
2017-04-06 15:21:38 -0400

    first commit skeleton

---
+++ test/Form.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import Form from '../src/Form';
+
+test('Should render Form', () => {
+  const form = shallow(
+    <Form/>
+  );
+  expect(form.text()).toEqual('Hello World');
+});
