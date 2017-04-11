f8aa730e4a54c33a600f55c71121e6bfcbf65b96
carohadad <carolinahadad@gmail.com>
2017-04-11 18:35:55 -0400

    Added firebase

--- src/Form.js
+++ src/Form.js
@@ -1,14 +1,84 @@
 import React from 'react';
+import AutoComplete from 'material-ui/AutoComplete';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+import * as firebase from 'firebase';
 import './Form.css';
 
-class Form extends React.Component {
+// Firebase config
+const config = {
+  apiKey: "AIzaSyCmkP2i7T_BuXYPIa3_kFkehOqo1yl5KVs",
+  authDomain: "icecream-world.firebaseapp.com",
+  databaseURL: "https://icecream-world.firebaseio.com",
+  projectId: "icecream-world",
+  storageBucket: "icecream-world.appspot.com",
+  messagingSenderId: "789735052164"
+};
+
+const database = firebase
+  .initializeApp(config)
+  .database()
+  .ref();
+
+
+class ConuntrySelector extends React.Component {
+
+  constructor() {
+    super();
+    var countries = require('country-list')();
+    this.state = {
+      dataSource: countries.getNames(),
+    };
+  }
+
   render() {
     return(
-      <div>Hello World</div>
+        <AutoComplete
+          hintText="Type your country name"
+          dataSource={this.state.dataSource}
+          floatingLabelText="Where are you from?"
+          fullWidth={true}
+          filter={AutoComplete.fuzzyFilter}
+          maxSearchResults={5}
+          onNewRequest={(countryName) => this.props.countrySelected(countryName)}
+        />
+    )
+  }
+}
+
+class Form extends React.Component {
+  constructor() {
+      super();
+      this.state = {
+        selectedCountry: null
+      };
+    }
+
+  countrySelected(countryName) {
+    this.setState({selectedCountry: countryName})
+  }
+
+
+  addVote() {
+    database.child('votes').push({country: this.state.selectedCountry});
+  };
+
+
+  render() {
+    return(
+      <div>
+        <div> </div>
+        <ConuntrySelector countrySelected={(countryName) => this.countrySelected(countryName)}/>
+        <div className='selectedCountry'>Selected Conuntry is {this.state.selectedCountry}</div>
+        <button onClick={this.addVote()}>
+          Vote
+        </button>
+
+
+      </div>
     )
   }
 
 }
-
 
 export default Form;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Form from './Form';
 
+const App = () => (
+  <MuiThemeProvider>
+    <Form />
+  </MuiThemeProvider>
+);
+
 ReactDOM.render(
-  <Form />,
+  <App />,
   document.getElementById('container')
 );
