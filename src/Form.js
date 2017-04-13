72492574d191258c3cd290125f9dcc8c9707e03d
carohadad <carolinahadad@gmail.com>
2017-04-13 12:28:26 -0400

    created skeleton for Flavours Selector and moved firebase-write functionality to a new method to submit both country and flavour

--- src/Form.js
+++ src/Form.js
@@ -38,16 +38,76 @@ class ConuntrySelector extends React.Component {
   }
 }
 
+class FlavoursSelector extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      dataSource : []
+    }
+  }
+
+  componentWillMount() {
+    this.firebaseFlavoursRef = databaseRef.ref("flavours");
+    this.firebaseFlavoursRef.once('value').then((snapshot) => {
+      var flavours = []
+      snapshot.forEach((elem) => {
+        flavours.push(elem.val())
+      })
+      this.setState({
+        dataSource: flavours
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.firebaseFlavoursRef.off();
+  }
+
+  dataSourceConfig() {
+    return { text: 'name', value: 'name'}
+  }
+
+  render() {
+    return (
+       <AutoComplete
+          hintText="Select your favourite Ice Cream flavours"
+          dataSource={this.state.dataSource}
+          dataSourceConfig={this.dataSourceConfig()}
+          floatingLabelText="Select your favourite Ice Cream flavours"
+          fullWidth={true}
+          filter={AutoComplete.fuzzyFilter}
+          maxSearchResults={5}
+        />
+    )
+  }
+}
+
 class Form extends React.Component {
   constructor() {
       super();
       this.state = {
         selectedCountry: null,
+        flavours:[]
       };
+      this.firebaseVotes = databaseRef.ref("votes");
+
     }
 
   countrySelected(countryName) {
-    databaseRef.ref("votes").push({country: countryName});
+    this.setState({
+      selectedCountry: countryName
+    })
+  }
+
+  flavoursSelected() {
+
+  }
+
+  submit() {
+    if (this.state.selectedCountry) {
+      this.firebaseVotes.push({country: this.state.selectedCountry, flavours: this.state.flavours});
+    }
   }
 
 
@@ -55,6 +115,8 @@ class Form extends React.Component {
     return(
       <div>
         <ConuntrySelector countrySelected={(countryName) => this.countrySelected(countryName)}/>
+        <FlavoursSelector flavoursSelected={(flavoursArray) => this.flavoursSelected(flavoursArray)} />
+        <RaisedButton label="Vote!" primary={true} onClick={()=> this.submit()}/>
       </div>
     )
   }
