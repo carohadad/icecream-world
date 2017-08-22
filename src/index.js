6261849dd1905cd1a1ae7a8107f60544d0d3bc58
carohadad <carolinahadad@gmail.com>
2017-08-22 10:49:48 -0300

    Added Results component to index

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Form from './Form';
 import DataPrev from './DataPrev';
+import Results from './Results';
 
 const App = () => (
   <MuiThemeProvider>
     <div>
       <DataPrev />
       <Form />
+      <Results />
     </div>
   </MuiThemeProvider>
 );
